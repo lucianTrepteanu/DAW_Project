bc700c8a56feee60ccfb43e0e0e0c012258e57b9
Lucian Trepteanu <lucian.trepteanu@gmail.com>
2021-01-09 18:37:20 +0200

    Admin fix

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -43,10 +43,11 @@ namespace Shop
                         userManager.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
                         userManager.CreateAsync(managerUser, "password").GetAwaiter().GetResult();
 
-                        var adminClaim = new Claim("Role","Admin");
+                        var adminClaim = new Claim("Role", "Admin");
                         var managerClaim = new Claim("Role", "Manager");
 
                         userManager.AddClaimAsync(adminUser, adminClaim).GetAwaiter().GetResult();
+                        userManager.AddClaimAsync(adminUser, managerClaim).GetAwaiter().GetResult();
                         userManager.AddClaimAsync(managerUser, managerClaim).GetAwaiter().GetResult();
                     };
                 };
